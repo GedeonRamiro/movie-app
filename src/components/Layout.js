8c799e3159e1678a7638e8d7b3187b0426764e86
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-01-13 14:10:58 -0300

    feat: init

---
+++ src/components/Layout.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+
+const Layout = ( {children} ) => {
+    return (
+        <>
+        <div className="mb-2 shadow-lg navbar bg-neutral text-neutral-content">
+            <div className="flex-none px-2 mx-2">
+                <span className="text-lg font-bold">
+                    Movie-App
+                </span>
+            </div> 
+            <div className="flex-1 px-2 mx-2">
+                <div className="items-stretch hidden lg:flex">
+                    <Link className="btn btn-ghost btn-sm rounded-btn"  to={'/'}> Home </Link>
+                    <Link className="btn btn-ghost btn-sm rounded-btn" to={'/sign-in'}> SignIn </Link>
+                </div>
+            </div> 
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">             
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
+                </svg>
+                </button>
+            </div>
+        </div>
+        {children}
+        </>
+
+    )
+}
+
+export default Layout
